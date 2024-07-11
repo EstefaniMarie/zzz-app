a5b8f9abce8299e99f3525afe2af0b2d5cd3d36c
Sheldon Youngo <sheldomyoungo2@gmail.com>
2024-07-11 14:39:36 -0400

    Staged traduccion

---
+++ app/Http/Controllers/TratamientosController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+
+class TratamientosController extends Controller
+{
+    public function index()
+    {
+        return view('tratamientos.index');
+    }
+}
