afd75d43a085471fabd5a272ac511fa599483630
EstefaniMarie <estefani98rosales@gmail.com>
2024-07-11 11:20:15 -0400

    recuperacion de contraseña

---
+++ resources/views/tratamientos/index.blade.php
@@ -0,0 +1,28 @@
+<x-app-layout>
+    <x-slot name="css">
+        <link rel="stylesheet" type="text/css"
+            href="{{ asset('theme/CryptoDash') }}/app-assets/vendors/css/forms/toggle/switchery.min.css">
+        <link rel="stylesheet" type="text/css"
+            href="{{ asset('theme/CryptoDash') }}/app-assets/css/pages/account-profile.css">
+
+    </x-slot>
+    <x-slot name="js">
+        <script src="{{ asset('theme/CryptoDash/app-assets/vendors/js/forms/toggle/switchery.min.js') }}"
+            type="text/javascript"></script>
+        <script src="{{ asset('theme/CryptoDash') }}/app-assets/js/scripts/forms/account-profile.js"
+            type="text/javascript">
+            </script>
+    </x-slot>
+    <x-slot name='header'>
+        <div class="content-header-left col-12 mb-2 breadcrumb-new">
+            <h3 class="content-header-title mb-0 d-inline-block">Atención primaria</h3>
+            <div class="row breadcrumbs-top d-inline-block">
+                <div class="breadcrumb-wrapper col-12">
+                    <ol class="breadcrumb">
+                        <li class="breadcrumb-item">Tratamientos</li>
+                    </ol>
+                </div>
+            </div>
+        </div>
+    </x-slot>
+</x-app-layout>
