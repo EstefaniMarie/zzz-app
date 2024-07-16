4b1209e2988e75276169338fc9964be478a6d8b1
EstefaniMarie <estefani98rosales@gmail.com>
2024-07-16 14:47:10 -0400

    asignacion de citas

--- app/Http/Controllers/CitasController.php
+++ app/Http/Controllers/CitasController.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Citas;
+use App\Models\Consultas;
 use App\Models\Medicos;
 use Database\Factories\CitasFactory;
 
@@ -12,18 +12,21 @@ class CitasController extends Controller
     public function index()
     {
         $medicos = Medicos::with('usuario.persona')->get();
-        $citas = Citas::with('persona.paciente')->first();
-        $cita = $citas->consulta;
-        dd($cita);
+        $consultas = Consultas::with('cita.persona.paciente.persona')->get();
 
         $eventos = [];
 
-        foreach ($citas as $cita) {
-            $eventos[] = [
-                'title' => $cita->consulta->paciente->nombres . ' ' . $cita->consulta->paciente->apellidos,
-                'start' => $cita->consulta->start_date->toIso8601String(),
-                'end'   => $cita->consulta->end_date->toIso8601String(),
-            ];
+        foreach ($consultas as $consulta) {
+            if ($consulta->cita) {
+                $start = new \DateTime($consulta->start_date);
+                $end = new \DateTime($consulta->end_date);
+                $evento = [
+                    'title' => $consulta->cita->paciente->nombres . ' ' . $consulta->cita->paciente->apellidos,
+                    'start' => $start->format(\DateTime::ISO8601), // Formato ISO8601
+                    'end'   => $end->format(\DateTime::ISO8601),
+                ];
+                $eventos[] = $evento;
+            }
         }
 
         return view('citas.index', compact('eventos', 'medicos'));
